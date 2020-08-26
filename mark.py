769d5b74c7aaeec0200c7b8842c1f19d40646651
Suyog Adhikari <suyogxda@gmail.com>
2020-08-26 14:07:24 +0545

    Add mark.py

--- mark.py
+++ mark.py
@@ -1,1 +1,24 @@
-__author__ = 'suyogxda'
+import argparse
+from watermark import Watermark
+
+parser = argparse.ArgumentParser(description='Script used to add watermark and reverse the process.')
+parser.add_argument('--image', '-i', help='Path to the input image.', default='sample/sample_image.png')
+parser.add_argument('--text', '-t', help='Text for watermark.', default='Monkey D Luffy')
+process_type = parser.add_mutually_exclusive_group(required=False)
+process_type.add_argument('--add', dest='add_watermark', action='store_true')
+process_type.add_argument('--remove', dest='add_watermark', action='store_false')
+parser.set_defaults(add_watermark=True)
+
+args = parser.parse_args()
+
+# Default parameters for Watermark class are:
+#     text='Sample watermark'
+#     alpha=0.3
+#     color=(0, 0, 255)
+#     path_to_save='./result'
+
+watermark = Watermark(text=args.text)
+if args.add_watermark:
+    watermarked_image = watermark.add_watermark(args.image)
+else:
+    un_watermarked_image = watermark.remove_watermark(args.image)
